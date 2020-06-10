e2a9dde28ff2f1b49dd8acedc1174fa062852b33
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-10 15:40:49 +0800

    Variables and the Assignment Operator

---
+++ lvc/variable.py
@@ -0,0 +1,14 @@
+# Working with Variables
+my_str = "This is a simple string"
+print(my_str)
+my_str += " testing"
+print(my_str)
+my_str = 1
+print(my_str)
+
+# Short-Hand Assignment Operators
+my_str = 1
+my_int = my_str
+my_str = "testing"
+print(my_int)
+print(my_str)
