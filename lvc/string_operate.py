10b91db3732e351901b2b70fe6781b2a86601b55
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-10 15:46:22 +0800

    Strings and String Operators

---
+++ lvc/string_operate.py
@@ -0,0 +1,25 @@
+# Strings
+print('single quoted string')
+print("double quoted string")
+print('''
+this is a triple
+quoted string
+''')
+
+print("pass" + "word")
+print("Ha" * 4)
+
+print("double".find('s'))
+print("double".find('u'))
+print("double".find('bl'))
+
+print("TeStInG".lower()) # "testing"
+print("another".lower())
+print("PassWord123".lower())
+
+print("Tab\tDelimited")
+print("New\nLine")
+print("Slash\\Character")
+print("'Single' in Double")
+print('"Double" in Single')
+print("\"Double\" in Double")
