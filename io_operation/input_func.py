004e266f30b85a0df53afe0cd10304074c7fa879
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-10 19:44:20 +0800

    The input Function

---
+++ io_operation/input_func.py
@@ -0,0 +1,7 @@
+# Prompting for Multiple Values
+name = input("What is your name? ")
+color = input("What is your favorite color? ")
+age = int(input("How old are you today? "))
+print(name)
+print(color)
+print(age)
