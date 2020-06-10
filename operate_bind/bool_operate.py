410e185447889b4b15c32e7f71db7776b4bc8651
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-10 18:05:12 +0800

    Boolean Operators

---
+++ operate_bind/bool_operate.py
@@ -0,0 +1,27 @@
+# The not Operation
+print(not True)
+print(not False)
+
+# The or Operation
+"""
+>>> True or True
+True
+>>> True or False
+True
+>>> False or False
+False
+>>> False or True
+True
+"""
+
+# The and Operation
+"""
+>>> True and True
+True
+>>> True and False
+False
+>>> False and False
+False
+>>> False and True
+False
+"""
