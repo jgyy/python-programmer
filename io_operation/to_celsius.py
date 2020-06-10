5ecdb3cbbb9da0abfea66464e7031e28bc34f3d5
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-10 20:03:40 +0800

    removed shebang as it dont work in windows environment

--- io_operation/to_celsius.py
+++ io_operation/to_celsius.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3.7
-
 # Python implementation here
 fahrenheit = float(input("What temperature (in Fahrenheit) would you like converted to Celsius? "))
 celsius = (fahrenheit - 32) * 5 / 9
