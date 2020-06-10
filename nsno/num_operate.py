3c382eb8041a6379350bd78c7a8a133cfd1c6947
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-10 16:37:01 +0800

    Numeric Operators

---
+++ nsno/num_operate.py
@@ -0,0 +1,8 @@
+# The Numeric Operators
+print(2 + 2) # Addition
+print(10 - 4) # Subtraction
+print(3 * 9) # Multiplication
+print(5 / 3) # Division, converts the result to a float even when evenly divided.
+print(5 // 3) # Floor division, always returns a number without a remainder as an int
+print(8 % 3) # Modulo division, returns the remainder as an int
+print(2 ** 3) # Exponent
