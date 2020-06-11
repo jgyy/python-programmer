092dd12cfa3787683bbed5bd0a9347dbcf0b841e
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-11 18:38:33 +0800

    removed the shebang again.

--- looping/fizz_buzz.py
+++ looping/fizz_buzz.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3.7
-
 upper_number = int(input("How many values should we process: "))
 
 for number in range(1, upper_number + 1):
@@ -11,4 +9,3 @@ for number in range(1, upper_number + 1):
       print("Buzz")
   else:
       print(number)
-
