71388724caf04975581f162f455670a866a94e5b
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-11 13:45:05 +0800

    String Indexing and Slicing

---
+++ soc/str_index.py
@@ -0,0 +1,13 @@
+# Indexing
+test_str = 'testing'
+print(test_str[0])
+print(test_str[len(test_str) - 1])
+print(test_str[-1])
+print(test_str[-4])
+
+# Slicing
+print(test_str[0:2])
+print(test_str[3:5])
+print(test_str[2:len(test_str)])
+print(test_str[2:])
+print(test_str[::-1])
