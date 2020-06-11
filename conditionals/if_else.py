811ce74694eb6346826736ca4b6e0a6fd95cb7ee
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-11 17:32:42 +0800

    The if and else Statements

---
+++ conditionals/if_else.py
@@ -0,0 +1,12 @@
+# The if and else Statements
+true = True
+false = False
+was_true = "Was True"
+if true:
+    print(was_true)
+if false:
+    print(was_true)
+if false:
+    print(was_true)
+else:
+    print("Was False")
