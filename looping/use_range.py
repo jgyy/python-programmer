600da6cf84e6caf614b662071cfade84e67c36d6
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-11 18:10:23 +0800

    Using ange

---
+++ looping/use_range.py
@@ -0,0 +1,13 @@
+# Ranges
+my_range = range(10)
+print(my_range)
+print(list(my_range))
+print(list(range(1, 14, 2)))
+
+count = 1
+while count <= 4:
+    print("looping")
+    count += 1
+
+for _ in range(1, 5):
+    print("looping")
