130eeb57fece3f89eaede241eeb2d0d2d463cd27
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-11 14:10:54 +0800

    Nested Lists: Matrices and Cubes

---
+++ lists/matric_cube.py
@@ -0,0 +1,8 @@
+# Creating a Matrix
+my_matrix = [[1, 2, 3], [4, 5, 6]]
+print(my_matrix)
+row_count = len(my_matrix)
+column_count = len(my_matrix[0])
+print(row_count)
+print(column_count)
+print(my_matrix[0][1])
