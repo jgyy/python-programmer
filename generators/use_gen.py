184a1775ee42f0b5f429a95b768daef128701ec2
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-11 22:04:05 +0800

    Converting Generators to Lists

--- generators/use_gen.py
+++ generators/use_gen.py
@@ -5,12 +5,22 @@ def gen_range(stop, start=1, step=1):
         yield num
         num += step
 
+def gen_fib():
+    a, b, c = 0, 1, 0
+    while c < 1024:
+        a, b, c = b, a + b, c + 1
+        yield (c, a)
+
 print(gen_range(10))
 generator = gen_range(4)
-print(next(generator))
-print(next(generator))
-print(next(generator))
-print(next(generator))
+print(list(generator))
 
 for num in gen_range(10, step=2):
     print(num)
+
+# Converting a Generator to a List
+generator = gen_range(10)
+print(list(generator))
+
+fib = gen_fib()
+print(list(fib))
