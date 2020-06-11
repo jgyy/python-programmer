61b25a7241165bffff34ff407509c8f675517fb1
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-11 15:01:36 +0800

    Dictionary Methods

---
+++ dictionaries/dict_method.py
@@ -0,0 +1,8 @@
+# Dictionary Methods
+ages = {'kevin': 61, 'bob': 79}
+print(ages.keys())
+print(list(ages.keys()))
+print(ages.values())
+print(list(ages.values()))
+print(ages.items())
+print(list(ages.items()))
