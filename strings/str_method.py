e397b209d78af10022b8c103b178e9425e00138d
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-11 17:09:53 +0800

    Useful String Methods, Part 1

---
+++ strings/str_method.py
@@ -0,0 +1,21 @@
+# Changing String Capitalization
+my_str = 'tEsTinG'
+print(my_str.lower())
+print(my_str.upper())
+print(my_str.capitalize())
+email = input("Your Email: ")
+print("Email is test@example.com:", email.lower() == 'test@example.com')
+
+# Checking String Patterns with .is___ Methods
+print("12".isascii())
+print("12".islower())
+print("12".isupper())
+print("12".istitle())
+print("12".isspace())
+print("12".isdecimal())
+print("12".isdigit())
+print("12".isnumeric())
+print("12".isalpha())
+print("12".isalnum())
+print("12".isidentifier())
+print("12".isprintable())
