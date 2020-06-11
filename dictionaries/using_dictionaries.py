374f8f55c54bd6ade2cb193105b5bbef9f6929ee
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-11 16:59:58 +0800

    refactored the emails as the string values are constantly repeated.

--- dictionaries/using_dictionaries.py
+++ dictionaries/using_dictionaries.py
@@ -1,35 +1,38 @@
+ashley_email = 'ashley@example.com'
+dalton_email = 'dalton@example.com'
+elizabeth_email = 'elizabeth@example.com'
 # 1) Set the emails variable to be an empty dictionary
 emails = {}
 
 assert emails == {}, f"Expected `emails` to be {{}} but got: {repr(emails)}"
 
 # 2) Add 'ashley', 'craig', and 'elizabeth' to the emails dictionary without reassigning the variable.
-emails['ashley'] = 'ashley@example.com'
+emails['ashley'] = ashley_email
 emails['craig'] = 'craig@example.com'
-emails['elizabeth'] = 'elizabeth@example.com'
+emails['elizabeth'] = elizabeth_email
 
 assert emails == {
-    "ashley": "ashley@example.com",
+    "ashley": ashley_email,
     "craig": "craig@example.com",
-    "elizabeth": "elizabeth@example.com",
-}, f"Expected `emails` to be {{'ashley': 'ashley@example.com', 'craig': 'craig@example.com', 'elizabeth': 'elizabeth@example.com'}} but got: {repr(emails)}"
+    "elizabeth": elizabeth_email,
+}, f"Expected `emails` to be {{'ashley': '{ashley_email}', 'craig': 'craig@example.com', 'elizabeth': '{elizabeth_email}'}} but got: {repr(emails)}"
 
 # 3) Remove 'craig' from the emails dictionary without reassigning the variable.
 del emails["craig"]
 
 assert emails == {
-    "ashley": "ashley@example.com",
-    "elizabeth": "elizabeth@example.com",
-}, f"Expected `emails` to be {{'ashley': 'ashley@example.com', 'elizabeth': 'elizabeth@example.com'}} but got: {repr(emails)}"
+    "ashley": ashley_email,
+    "elizabeth": elizabeth_email,
+}, f"Expected `emails` to be {{'ashley': '{ashley_email}', 'elizabeth': '{elizabeth_email}'}} but got: {repr(emails)}"
 
 # 4) Add 'dalton' to the emails dictionary without reassigning the variable.
-emails["dalton"] = "dalton@example.com"
+emails["dalton"] = dalton_email
 
 assert emails == {
-    "ashley": "ashley@example.com",
-    "elizabeth": "elizabeth@example.com",
-    "dalton": "dalton@example.com",
-}, f"Expected `emails` to be {{'ashley': 'ashley@example.com', 'elizabeth': 'elizabeth@example.com', 'dalton': 'dalton@example.com'}} but got: {repr(emails)}"
+    "ashley": ashley_email,
+    "elizabeth": elizabeth_email,
+    "dalton": dalton_email,
+}, f"Expected `emails` to be {{'ashley': '{ashley_email}', 'elizabeth': '{elizabeth_email}', 'dalton': '{dalton_email}'}} but got: {repr(emails)}"
 
 # 5) Return a list of keys from the emails dictionary as `users`
 users = list(emails.keys())
@@ -44,16 +47,16 @@ assert users == [
 email_list = list(emails.values())
 
 assert email_list == [
-    "ashley@example.com",
-    "elizabeth@example.com",
-    "dalton@example.com",
-], f"Expected `email_list` to be ['ashely@example.com', 'elizabeth@example.com', 'dalton@example.com'] but got: {repr(email_list)}"
+    ashley_email,
+    elizabeth_email,
+    dalton_email,
+], f"Expected `email_list` to be ['ashely@example.com', '{elizabeth_email}', '{dalton_email}'] but got: {repr(email_list)}"
 
 # 7) Return a list of tuples called `pairs` representing the key/value pairs in `emails`.
 pairs = list(emails.items())
 
 assert pairs == [
-    ("ashley", "ashley@example.com"),
-    ("elizabeth", "elizabeth@example.com"),
-    ("dalton", "dalton@example.com"),
-], f"Expected `pairs` to be [('ashley', 'ashley@example.com'), ('elizabeth', 'elizabeth@example.com'), ('dalton', 'dalton@example.com')] but got: {repr(pairs)}"
+    ("ashley", ashley_email),
+    ("elizabeth", elizabeth_email),
+    ("dalton", dalton_email),
+], f"Expected `pairs` to be [('ashley', '{ashley_email}'), ('elizabeth', '{elizabeth_email}'), ('dalton', '{dalton_email}')] but got: {repr(pairs)}"
