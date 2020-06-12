4645bfd6264c10a86c037de3ee97bcfb94f9a00b
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-12 23:32:37 +0800

    Python programmer test question 1

---
+++ test30.py
@@ -0,0 +1,3 @@
+# Which of the following lines of code would set the third value in the names list to be 'Jimmy'? Here's the starting value for names:
+names = ['Alice', 'Bob', 'Lance', 'Mike']
+print(names)
