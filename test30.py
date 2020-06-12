579fdfcbf8e57d314ad0b0fdf36b97ef6c5e1891
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-12 23:34:57 +0800

    Python programmer test question 4

--- test30.py
+++ test30.py
@@ -1,3 +1,13 @@
 # Which of the following lines of code would set the third value in the names list to be 'Jimmy'? Here's the starting value for names:
 names = ['Alice', 'Bob', 'Lance', 'Mike']
 print(names)
+
+# What is the output of this code?
+num1 = 15
+num2 = num1
+num1 *= 2
+print(num2)
+
+# "#" is used to create a single-line comment.
+
+# '7.2' and 'Test' is a string?
