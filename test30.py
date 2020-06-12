4fa27aabb09fda7081a92d86d4fd84c46e735a00
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-12 23:35:34 +0800

    Python programmer test question 5

--- test30.py
+++ test30.py
@@ -11,3 +11,9 @@ print(num2)
 # "#" is used to create a single-line comment.
 
 # '7.2' and 'Test' is a string?
+
+# What would be the value of the all_names variable after running this code?
+names = ['Alice', 'Lance', 'Bob', 'Mike']
+all_names = names
+names.append('Brock')
+print(all_names)
